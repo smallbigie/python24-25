440f2f646b304845b3af69ed10cdba1315ec48ed
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2024-11-20 15:15:09 +0000

    wed 11/20 programming example

---
+++ animalChooser.py
@@ -0,0 +1,43 @@
+animal = input("Whats your favorite animal:Dog, Cat, Lion, Tiger, Rabbit, Bird, Dolphin, Elephant, Turtle, Shark?: ")
+
+if animal == "Dog":
+    print("Ah, so your favorite animal is a Dog. Let's see what that says about you! ")
+    print("Based on your answer, you are very playful, silly, and loyal")
+
+elif animal == "Cat":
+    print("Ah, so your favorite animal is a Cat. Let's see what that says about you! ")
+    print("Based on your answer, you are very swift and coordinated")
+
+elif animal == "Lion":
+    print("Ah, so your favorite animal is a Lion. Let's see what that says about you! ")
+    print("Based on your answer, you are very Brave and you lead others around you")
+
+elif animal == "Tiger":
+    print("Ah, so your favorite animal is a Tiger. Let's see what that says about you! ")
+    print("Based on your answer, you are very calm and careful")
+
+elif animal == "Rabbit":
+    print("Ah, so your favorite animal is a Rabbit. Let's see what that says about you! ")
+    print("Based on your answer, you are very social and fast")
+
+elif animal == "Bird":
+    print("Ah, so your favorite animal is a Bird. Let's see what that says about you! ")
+    print("Based on your answer, you are very outgoing and adventurous")
+
+elif animal == "Dolphin":
+    print("Ah, so your favorite animal is a Dolphin. Let's see what that says about you! ")
+    print("Based on your answer, you are very thoughtful and kind-hearted")
+
+elif animal == "Elephant":
+    print("Ah, so your favorite animal is a Elephant. Let's see what that says about you! ")
+    print("Based on your answer, you are very smart and you think things out before doing it")
+
+elif animal == "Turtle":
+    print("Ah, so your favorite animal is a Turtle. Let's see what that says about you! ")
+    print("Based on your answer, you are very slow and careful ")
+
+elif animal == "Shark":
+    print("Ah, so your favorite animal is a Shark. Let's see what that says about you! ")
+    print("Based on your answer, you are very short tempered and violent")
+else:
+    print("This input is not valid")
