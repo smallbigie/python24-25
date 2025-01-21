d02f55fa1621b999ffe98fe7cbd6808ee08ece44
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2025-01-21 13:59:43 +0000

    tiny first tuesday update

---
+++ variableScopes.py
@@ -0,0 +1,9 @@
+"""
+Define a couple different types of variables in terms of their scope.
+"""
+
+
+# Global Variable -
+global = "I am a global variable and I am accessible everywhere."
+
+def outer_function():
