dd3a87d9c5aef35489b09055915475cf6d716ad3
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2025-01-21 15:51:47 +0000

    Variable scopes update

--- variableScopes.py
+++ variableScopes.py
@@ -2,8 +2,27 @@
 Define a couple different types of variables in terms of their scope.
 """
 
+import wedFuncRetBellRinger
 
 # Global Variable -
 global = "I am a global variable and I am accessible everywhere."
+name = "Cameron"
 
 def outer_function():
+    # Enclosing scope variable
+    enclosing_ver = "I am an enclosing variable and accessible to nested functions."
+
+    def inner_function(): # Nested Function
+        # Local variables
+        local_var = "I am a local variable and only accessible withing this function"
+
+        """
+        local_var is only available inside of the inner_function Namespace
+
+        enclosing_var is available inside of the outer_function Namespace and the inner_function Namespace
+
+        global_var is available in all of the namespaces created inside of this script. It would also be available inside of any script that imports the variableScopes.py
+        """
+    
+
+print(result)
