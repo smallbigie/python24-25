4bd4f09fc1e4f710b0198c7464b396c06cb817f5
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2025-01-23 15:44:45 +0000

    Thursday 1/23/25 update

---
+++ sumCalcExample.py
@@ -0,0 +1,14 @@
+"""
+This program that takes five numbers from the user and calculates their sum.
+"""
+
+num1 = input("Please enter your first number: ")
+num2 = input("Please enter your second number: ")
+num3 = input("Please enter your third number: ")
+num4 = input("Please enter your fourth number: ")
+num5 = input("Please enter your last number: ")
+
+try:
+     = int(input("Please enter your : "))
+except ValueError: 
+    print("")
